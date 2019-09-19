14fb7200e51bc4ec6cd6932426446e79251add4c
andrers <andrers@TRT-71093.trt3.jus.br>
2019-09-19 16:43:06 -0300

    commit aula 4

--- homeBanking/src/main/java/cursoJava/homeBanking/ClienteRepository.java
+++ homeBanking/src/main/java/cursoJava/homeBanking/ClienteRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
 
-@RepositoryRestResource(collectionResourceRel = "cliente", path = "cliente")
+@RepositoryRestResource(collectionResourceRel = "clientes", path = "clientes")
 @Component
 public interface  ClienteRepository extends PagingAndSortingRepository<Cliente, Long> {
 
